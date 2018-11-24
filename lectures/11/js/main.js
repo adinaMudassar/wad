10a81590ac0b0442d070de35dc27eba12e0c88c2
adinaMudassar <adiina.mudassar@ucp.edu.pk>
2018-11-24 22:03:00 +0500

    class activity lecture 11

--- lectures/11/js/main.js
+++ lectures/11/js/main.js
@@ -1,13 +1,14 @@
 //============================================
 //-----------  STEP - 1 ----------------------
 //============================================
-
-
-/*var todos = [];
+/*
+var todos = [];
 function add() {
     var task = document.getElementById("task").value;
     todos.push(task);
     document.getElementById('todos').innerText = todos;
+>>>>Ans:
+    It writes the value entered in the task on the onto the document, after add button is pressed.
 }*/
 
 
@@ -15,8 +16,9 @@ function add() {
 //-----------  STEP - 2 ----------------------
 //============================================
 
+/*
 
-/*var todos = [];
+var todos = [];
 function add() {
     var task = document.getElementById('task').value;
     todos.push(task);
@@ -34,15 +36,17 @@ function show() {
         ul.appendChild(li);
     }
     document.getElementById('todos').appendChild(ul);
-}*/
-
+}
+ >>>>>Ans:
+ same as before adds the value entered and displays them but this with borders around it  and displays the previous value when new value added.
+*/
 
 //============================================
 //-----------  STEP - 3 ----------------------
 //============================================
-
-
 /*
+
+
 function getTodos() {
     var todos = [];
     var todos_str = localStorage.getItem('todo');
@@ -84,6 +88,9 @@ function show() {
     document.getElementById('todos').appendChild(ul);
 }
 show();
+
+>>>>>>Ans:
+this time it displays the items entered and gives a delete button with them, even though it dose not perform the delete operation as of yet.
 */
 
 
@@ -91,9 +98,10 @@ show();
 //============================================
 //-----------  STEP - 4 ----------------------
 //============================================
+/*
 
 
-/*function getTodos() {
+function getTodos() {
     var todos = [];
     var todos_str = localStorage.getItem('todo');
     if(todos_str !== null)
@@ -162,10 +170,11 @@ function isDone(e) {
     localStorage.setItem('todo',JSON.stringify(todos));
     show();
 }
-show();*/
+show();
 
+>>>>>Ans:it displays the items entered and gives a delete button with them,now we can delete the previously added item, when delete button on item is pressed.
 
-
+*/
 
 
 //============================================
@@ -185,3 +194,98 @@ HINT: use text-decoration:line-through; property of CSS
 
 */
 
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task, isDone: false});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos.splice(id,1);
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' + todos[i].task + '</li>' +
+            '<button class="btn btn-danger" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>'+
+            '<button class="btn btn-reset" id="' + i + '">' +
+            '<i class="fa fa-redo"></i> ' +
+            '<span class="d-none d-sm-inline"> Replace </span> </button>'
+        li.classList.add("list-group-item");
+        if(todos[i].isDone)
+            li.classList.add("done");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    var buttons1 = document.getElementsByClassName('btn-reset');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+        buttons1[i].addEventListener('click',replaceElement);
+    }
+}
+function isDone(e) {
+    var todos = getTodos();
+    if(todos[e.target.id].isDone) {
+        e.target.classList.add('done');
+        todos[e.target.id].isDone = false;
+    }
+    else{
+        e.target.classList.remove('done');
+        todos[e.target.id].isDone = true;
+    }
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+show();
+function replaceElement() {
+    var id = this.getAttribute('id1');
+    var task = document.getElementById('task').value;
+    if(task.trim()=='')
+    {
+        document.getElementById('message').style.display = 'block';
+        return false;
+    }
+
+    else
+    {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.splice(id,1);
+    todos.push({task: task, isDone:false});
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+show();
